99ab40d41ad8b3c9fc89c20879c5c4d5da2358eb
王志洋 <wzy9609@126.com>
2019-10-17 14:16:42 +0800

    整理RequestHTML类结构

--- reptile_util.py
+++ reptile_util.py
@@ -1,25 +1,13 @@
 import requests
 from lxml import etree
-import sys
 from PIL import Image
 from io import BytesIO
 import os
+from fake_useragent import UserAgent
 
 
 # 请求网页
 class RequestHTML(object):
-
-    # 浏览器请求头
-    header = {'Connection': 'keep-alive',
-              'Cache-Control': 'max-age=0',
-              'Upgrade-Insecure-Requests': '1',
-              'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36',
-              'Sec-Fetch-Mode': 'navigate',
-              'Sec-Fetch-User': '?1',
-              'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-              'Sec-Fetch-Site': 'cross-site',
-              'Accept-Encoding': 'gzip, deflate, br',
-              'Accept-Language': 'zh-CN,zh;q=0.9'}
 
     # 实例化时输入网址
     def __init__(self, base_url):
@@ -32,52 +20,64 @@ class RequestHTML(object):
         # 图片前缀补零使用
         self.__name_length = 5
 
+    # 重置URL
     def set_url(self, url):
         self.__url = url
 
     # get方式请求网页
-    def get_html(self, encoding=None):
+    def get_request(self, params=None, headers=None, encoding='UTF-8'):
+        # 添加请求头
+        if headers is None:
+            headers = self.get_radom_headers()
         # get请求
-        html = requests.get(self.__url, headers=self.header, cookies=self.__cookies)
+        request = requests.get(self.__url, params=params, headers=headers, cookies=self.__cookies)
         # 保存Cookie
-        self.__cookies = html.cookies
-        if encoding is not None:
-            html.encoding = encoding
-        return html
+        self.__cookies = request.cookies
+        # 设置编码
+        request.encoding = encoding
+        return request
 
     # post方式请求网页
-    def post_html(self, data=None, json=None, url=None):
+    def post_request(self, data=None, json=None, headers=None, encoding='UTF-8'):
+        # 添加请求头
+        if headers is None:
+            headers = self.get_radom_headers()
         # post请求
-        if url is None:
-            html = requests.post(self.__url, data=data, json=json, headers=self.header, cookies=self.__cookies)
-        else:
-            html = requests.post(url, data=data, json=json, headers=self.header, cookies=self.__cookies)
+        request = requests.post(self.__url, data=data, json=json, headers=headers, cookies=self.__cookies)
         # 保存Cookie
-        self.__cookies = html.cookies
-        return html
+        self.__cookies = request.cookies
+        # 设置编码
+        request.encoding = encoding
+        return request
+
+    # 保存一张图片到本地
+    def save_img(self, local_path):
+        request = self.get_request()
+        img_name = os.path.split(self.__url)[1]
+        image = Image.open(BytesIO(request.content))
+        image.save(os.path.join(local_path, img_name))
+        print('保存完成')
 
     # 保存文件到本地
-    def save_img(self, local_path, img_urls=None):
-        # img_urls传递为list，用于批量保存图片
-        if img_urls is not None:
-            for img_url in img_urls:
-                response = requests.get(img_url, headers=self.header, cookies=self.__cookies)
-                img_name = os.path.split(img_url)[1]
-                image = Image.open(BytesIO(response.content))
-                index = str(self.__name_index)
-                num = self.__name_length - len(index)
-                if 0 > num:
-                    num = self.__name_length - 1
-                    self.__name_index = 1
-                    index = 1
-                self.__name_index = self.__name_index + 1
-                image.save(os.path.join(local_path, '0' * num + index + '_' + img_name))
-        else:
-            # 调用时没有传图片url时，视为实例化时的base_url参数，用于保存一张图片
-            response = self.get_html(self.__url)
-            img_name = os.path.split(self.__url)[1]
-            image = Image.open(BytesIO(response.content))
-            image.save(os.path.join(local_path, img_name))
+    def save_img_batch(self, local_path, img_urls):
+        for img_url in img_urls:
+            self.set_url(img_url)
+            request = self.get_request()
+            img_name = os.path.split(img_url)[1]
+            image = Image.open(BytesIO(request.content))
+            index = str(self.__name_index)
+            num = self.__name_length - len(index)
+            if 0 > num:
+                num = self.__name_length - 1
+                self.__name_index = 1
+                index = 1
+            self.__name_index = self.__name_index + 1
+            image.save(os.path.join(local_path, '0' * num + index + '_' + img_name))
+        print('保存完成')
+
+    # 随机生成请求头
+    def get_radom_headers(self):
+        return {'User-Agent': UserAgent().random}
 
 
 # 解析网页
@@ -95,25 +95,3 @@ class PathHTML(object):
         # 通过xpath定位要获取的节点或属性（谷歌安装个xpath插件很方便）
         documents = self.__html.xpath(xpath)
         return documents
-
-
-# 根据url与xpath获取对应document
-def main(url, xpath):
-    requestHTML = RequestHTML(url)
-    pathHTML = PathHTML(requestHTML.get_html().text, url)
-    documents = pathHTML.get_documents(xpath)
-    print(documents)
-
-
-if __name__ == '__main__':
-    args = sys.argv
-    if 3 > len(args):
-        print('请输入参数：1.url 2.xpath，如：')
-        print(args[0], 'url', 'xpath')
-    else:
-        url = args[1]
-        xpath = args[2]
-        main(url, xpath)
-        print('Done')
-
-
