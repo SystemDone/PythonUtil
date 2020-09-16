43bf594125c9d7dc913c5113d2dfa535ffbbeda9
wangzhiyang <wzy9609@126.com>
2020-09-16 11:14:10 +0800

    添加邮件抄送

--- mail_util.py
+++ mail_util.py
@@ -9,8 +9,9 @@ class MailUtil(object):
     user = '1462232055@qq.com'
     password = 'rfnupleuewmeiedb'
 
-    def __init__(self, to_mail):
+    def __init__(self, to_mail, cc_mail=None):
         self.__to_mail = to_mail
+        self.__cc_mail = cc_mail
         self.__msg = None
 
     def mail_content(self, title, content, subtype=None, charset=None):
@@ -20,10 +21,15 @@ class MailUtil(object):
             self.__msg = MIMEText(content)
         self.__msg['Subject'] = title
         self.__msg['From'] = self.sender
-        self.__msg['To'] = self.__to_mail[0]
+        self.__msg['To'] = ';'.join(self.__to_mail)
+        if self.__cc_mail is not None:
+            self.__msg['Cc'] = ';'.join(self.__cc_mail)
         return self
 
     def send(self):
         server = smtplib.SMTP_SSL(self.host, 465, timeout=2)
         server.login(self.sender, self.password)
-        server.sendmail(self.sender, self.__to_mail, self.__msg.as_string())
+        if self.__cc_mail is not None:
+            server.sendmail(self.sender, self.__to_mail + self.__cc_mail, self.__msg.as_string())
+        else:
+            server.sendmail(self.sender, self.__to_mail, self.__msg.as_string())
